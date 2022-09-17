1aafe3171cf4beefa5e9aaef4bc2bc047e4061f2
Adam <adamjamesgray.1992@gmail.com>
2022-09-17 23:53:00 +0100

    Created layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
+import About from './pages/About';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
 function App() {
   return (
-    <div className='bg-purple-500'>
-      <h1 className='text-xl'>Hello World</h1>
-      <button className='btn'>Cancel</button>
-    </div>
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='*' element={<NotFound />} />
+            <Route path='/'>
+              <Route index element={<Home />} />
+              <Route path='about' element={<About />} />
+            </Route>
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
